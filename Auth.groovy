eda6f5a1044e8bb63458bc563a4d8aab373daa54
ShawReav <97753163+ShawReav@users.noreply.github.com>
2022-04-30 10:57:12 +0530

    Create Auth.groovy

---
+++ Auth.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+// Install SFPowerkit
+pipeline {
+  agent any
+  stages {
+    stage('Authorize an Org') {
+      steps {
+        echo y | ${toolbelt}sfdx plugins:install sfpowerkit
+      }
+    }
+  }
+}
